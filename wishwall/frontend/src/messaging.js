6e4cfa0195637b08cbad44b30a84524b729c3b45
AayushKumarMishra-code <aayushkumarmishrao6@gmail.com>
2026-05-05 00:39:39 +0530

    updated the ui whole ui of messaging

--- wishwall/frontend/src/messaging.js
+++ wishwall/frontend/src/messaging.js
@@ -1,18 +1,49 @@
-const arr = [];
+let arr = [];
+
+
+
 document.querySelector("#send").addEventListener("click" , function(e){
-    let data = document.getElementById("info").value
-    arr.push(data);
-    let dl = document.querySelector("#message-dl");
-    dl.textContent = " "
-
+    let data = document.querySelector("#inp").value
+    console.log(data);
+    
+    arr = [...arr , data]
+    let message = document.querySelector("#message");
+    document.getElementById("inp").value= " ";
+    message.innerHTML = " "
+    
     arr.forEach(function(alpha){
-        
-        let dt = document.createElement("dt");
-        dt.innerHTML=`<br>Aayush <dd>${alpha}</dd>`
-        dl.append(dt);
+        let fieldset = document.createElement("fieldset")
+        fieldset.style.cssText = ` max-width: 70rem;
+            font-size: 2rem;
+            border-radius: 2rem;
+            margin-top:2rem;
+            border: 0.1rem solid rgb(245, 174, 174);
+            overflow-wrap: anywhere;`
+        fieldset.innerHTML +=`<legend style = "color : white; ">Aayush</legend>
 
+
+        <p id="actual-message">${alpha} </p>
+        <button id = "emoji" style = " font-size:2rem; border-radius:2rem"><span>☺️</span></button>
+        
+        
+        <select id="sel">
+        <option value="😂">😂</option>
+        <option value="❤️">❤️</option>
+        <option value="😭">😭</option>
+        <option value="😎">😎</option>
+        </select>`
+        
+        message.append(fieldset)
+        
+        
+    })
+    
+    //below logic is for reacting
+    document.querySelector("#sel").addEventListener("change", function(e){
+       document.querySelector("#emoji").innerHTML = e.target.value;
     })
     
     
 
 })
+
