5adde26df0f58a4d3df0d6c79b11cfbbcccaa1f0
AayushKumarMishra-code <aayushkumarmishrao6@gmail.com>
2026-05-11 20:25:22 +0530

    Co-authored-by: Aryan. <Master-Gamer-glitch@users.noreply.github.com>

--- wishwall/frontend/src/messaging.js
+++ wishwall/frontend/src/messaging.js
@@ -1,49 +1,84 @@
-let arr = [];
+let messageData = [{userName: "Aayush",userId: "23456", messageId: "9009",message:" hi we are here"},{userName: "kkkkkk",userId: "09999", messageId: "9010", message:" hi we are here"}];
+let usersId =["2345","23456","09999"];
+let currentUserId = usersId[usersId.length-1] // from local storage/jwt
+let msg = 100000;                               //todo 2: fetch from db...;
+let updateWalaUserName;
+let updateWalaUserId;
+let ExistingmessageId =null;
 
-
-
-document.querySelector("#send").addEventListener("click" , function(e){
-    let data = document.querySelector("#inp").value
-    console.log(data);
-    
-    arr = [...arr , data]
-    let message = document.querySelector("#message");
-    document.getElementById("inp").value= " ";
-    message.innerHTML = " "
-    
-    arr.forEach(function(alpha){
-        let fieldset = document.createElement("fieldset")
-        fieldset.style.cssText = ` max-width: 70rem;
-            font-size: 2rem;
-            border-radius: 2rem;
-            margin-top:2rem;
-            border: 0.1rem solid rgb(245, 174, 174);
-            overflow-wrap: anywhere;`
-        fieldset.innerHTML +=`<legend style = "color : white; ">Aayush</legend>
-
-
-        <p id="actual-message">${alpha} </p>
-        <button id = "emoji" style = " font-size:2rem; border-radius:2rem"><span>☺️</span></button>
-        
-        
-        <select id="sel">
-        <option value="😂">😂</option>
-        <option value="❤️">❤️</option>
-        <option value="😭">😭</option>
-        <option value="😎">😎</option>
-        </select>`
-        
-        message.append(fieldset)
-        
-        
-    })
-    
-    //below logic is for reacting
-    document.querySelector("#sel").addEventListener("change", function(e){
-       document.querySelector("#emoji").innerHTML = e.target.value;
-    })
-    
-    
-
+let currentUser = messageData.filter(function(messageData){         //todo 1: fix bug;
+    return messageData.userId == currentUserId;
 })
 
+//userId, userName == local storage. //fetch(..backend..)
+
+//local storage: JWT (userId), pfp as blob link, userName
+
+// createMessage, createUser, login, register, 
+
+// currentuser ke liye ek arr chahiye........ => userName , id [FROM LOCAL STORAGE]
+//
+
+
+
+
+
+document.querySelector("#send").addEventListener("click", function(e){
+    ++msg;
+    const{value:recentMessage}= document.getElementById("inp");
+    
+    const{userName:currentUserName}= currentUser[currentUser.length -1]//reason for this line to access the current userName // todo 3: fetch userId from localStorage in global scope;
+// check wheather a id is already present or not!!!!!!!
+    if(ExistingmessageId==currentUserId){
+
+        messageData = [...messageData,{userName:currentUserName,userId:currentUserId,messageId: msg, message :recentMessage }]
+    }
+    else{
+        messageData = [...messageData,{userName:updateWalaUserName ,userId:updateWalaUserId ,messageId: ExistingmessageId , message :recentMessage }]
+        ExistingmessageId = null;
+    }
+    render();
+})
+
+
+
+
+//delete message
+
+function deletemsg(messageId){
+    messageData= messageData.filter(function(messageData){
+        return messageData.messageId != messageId
+
+    })
+    render();
+
+}
+
+//updation of message
+
+function update(messageId){
+    const{UserName ,userId ,messageId , message} = messageData.find(function(messageData){
+        return messageData.messageId == messageId;
+    })
+
+    document.querySelector("#inp").value = message;
+    updateWalaUserName=UserName;
+    updateWalaUserId = userId;
+    
+    
+    
+}
+
+function render(){
+    messages.forEach(msg, function (msg){
+        // if(img): square layout..  else {html element img placeholder => delete;}
+    })
+}
+
+//Current Pending tasks.
+
+// 1. jwt implementation
+// 2. render logic: (fetch array from backend) + support for image as message. => HTML CARDS
+// 3. query router for backend 
+// ework is laid only implementation is left [second priority]
+// 4.  
